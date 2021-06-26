c19578d04113112a3d7e773f233c127724ac31b7
onghuilam <onghuilam@gmail.com>
2021-06-26 14:55:45 +0800

    init commit

---
+++ src/hl/opencv/OpenCvLibLoader.java
@@ -0,0 +1,137 @@
+/*
+ Copyright (c) 2021 onghuilam@gmail.com
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ The Software shall be used for Good, not Evil.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+ */
+
+package hl.opencv;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import org.opencv.core.Core;
+
+import hl.common.FileUtil;
+
+public class OpenCvLibLoader{
+	
+	private String opencv_jarpath 	= null;
+	private String native_libpath 	= null;
+	private String native_libname 	= null;
+	private ClassLoader classLoader = null;
+	private boolean loaded 			= false;
+	private Exception errInit		= null;
+	private String opencv_version	= null;
+
+	public OpenCvLibLoader(String NativeLibName, String NativeLibPath)
+	{
+		setNative_libname(NativeLibName);
+		setNative_libpath(NativeLibPath);
+	}
+	//
+	public String getOpencv_jarpath() {
+		return opencv_jarpath;
+	}
+	public void setOpencv_jarpath(String opencv_jarpath) {
+		this.opencv_jarpath = opencv_jarpath;
+	}
+	//
+	public String getNative_libpath() {
+		return native_libpath;
+	}
+	public void setNative_libpath(String native_libpath) {
+		this.native_libpath = native_libpath;
+	}
+	//
+	public String getNative_libname() {
+		return native_libname;
+	}
+	public void setNative_libname(String native_libname) {
+		this.native_libname = native_libname;
+	}
+	//
+	public ClassLoader getClassLoader() {
+		return classLoader;
+	}
+	public void setClassLoader(ClassLoader classLoader) {
+		this.classLoader = classLoader;
+	}
+	//
+	public Exception getInitException()
+	{
+		return this.errInit;
+	}
+	//
+	public String getVersion()
+	{
+		return this.opencv_version;
+	}
+	//
+	public boolean init()
+	{
+		try {
+			this.errInit= null;
+			this.loaded = FileUtil.loadNativeLib(this.native_libname, this.native_libpath);
+			
+			ClassLoader cl = OpenCvLibLoader.class.getClassLoader();
+			
+			if(getOpencv_jarpath()!=null)
+			{
+				File fileJar = new File(getOpencv_jarpath());
+				if(fileJar.isFile())
+				{
+					cl = new URLClassLoader(new URL[]{ new URL(fileJar.getCanonicalPath())});
+				}
+				else
+				{
+					throw new Exception("File NOT found ! - "+getOpencv_jarpath());
+				}
+			}
+			
+			Class<?> classCV = cl.loadClass("org.opencv.core.Core");
+			if(classCV!=null)
+			{
+				Field f = classCV.getDeclaredField("VERSION");
+				if(f!=null)
+				{
+					this.opencv_version = (String) f.get(null);
+				}
+			}
+			
+			
+		} catch (Exception e) {
+			this.loaded = false;
+			this.errInit = e;
+		}
+		return this.loaded;
+	}
+	
+	public static void main(String args[]) throws Exception
+	{
+		File curDir = new File(".");
+		//
+		String sNativeLibPath = "./"+curDir.getCanonicalPath()+"/lib/opencv/4.4.0";
+		String sNativeLibName = Core.NATIVE_LIBRARY_NAME;
+		OpenCvLibLoader cvLoader = new OpenCvLibLoader(sNativeLibName, sNativeLibPath);
+		System.out.println("Loaded OpenCV Version : "+(cvLoader.init()?cvLoader.getVersion():null));
+		//
+	}
+}
---
+++ src/hl/opencv/util/OpenCvUtil.java
@@ -0,0 +1,312 @@
+/*
+ Copyright (c) 2021 onghuilam@gmail.com
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ The Software shall be used for Good, not Evil.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+ */
+
+package hl.opencv.util;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.File;
+import java.util.Vector;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+public class OpenCvUtil{
+	
+	public static Mat bufferedImg2Mat(BufferedImage img)
+	{
+		Mat mat = null;
+		if(img!=null)
+		{
+			img = removeAlphaChannel(img);
+			byte[] data = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+	        mat = new Mat(img.getHeight(), img.getWidth(), CvType.CV_8UC3);
+	        mat.put(0, 0, data);
+		}
+        return mat;
+	}
+	
+	public static BufferedImage mat2BufferedImage(Mat aMat){
+		int type = BufferedImage.TYPE_BYTE_GRAY;
+		switch(aMat.channels())
+		{
+			case 3 : type = BufferedImage.TYPE_3BYTE_BGR; break;
+			case 4 : type = BufferedImage.TYPE_4BYTE_ABGR; break;
+		}
+		int bufferSize = aMat.channels()* aMat.cols()* aMat.rows();
+		byte [] b = new byte[bufferSize];
+		aMat.get(0,0,b); // get all the pixels
+		BufferedImage image = new BufferedImage(aMat.cols(), aMat.rows(), type);
+		final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		System.arraycopy(b, 0, targetPixels, 0, b.length);  
+		return image;
+	}
+	//
+	
+	public static Mat resizeByWidth(final Mat aMatImg, int aNewWidth)
+	{
+		double dImageW = (double)aMatImg.width();
+		double dImageH = (double)aMatImg.height();
+		
+		double dScaleW 	= aNewWidth>0 ? ((double)aNewWidth) / dImageW : 1.0;
+		int iNewHeight =(int)(dImageH * dScaleW);
+
+		return resize(aMatImg, aNewWidth, iNewHeight, false);
+	}
+		
+	public static Mat resize(final Mat aMatImg, int aNewWidth, int aNewHeight, boolean isMainAspectRatio)
+	{
+		if(isMainAspectRatio)
+		{
+			double dImageW = (double)aMatImg.width();
+			double dImageH = (double)aMatImg.height();
+			
+			double dScaleW 	= (aNewWidth>0)&&(dImageW>0) ? ((double)aNewWidth) / dImageW : 1.0;
+			double dScaleH 	= (aNewHeight>0)&&(dImageH>0) ? ((double)aNewHeight) / dImageH : 1.0;
+			double dScale = dScaleW>dScaleH ? dScaleH : dScaleW;
+			
+			aNewWidth = (int)(dImageW * dScale);
+			aNewHeight =(int)(dImageH * dScale);
+			System.out.println("dScale="+dScale);
+		}
+		
+		System.out.println("aNewWidth="+aNewWidth);
+		System.out.println("aNewHeight="+aNewHeight);
+		
+		Mat matSized = new Mat();
+		Imgproc.resize(aMatImg, matSized, new Size(aNewWidth, aNewHeight));
+		return matSized;
+	}
+	//
+	
+	public static Mat extractForeground(Mat matInput, Mat matBackground, double aDiffThreshold) throws Exception
+	{
+		if(aDiffThreshold<0 && aDiffThreshold>1)
+		{
+			throw new Exception("Threshold value should be 0.0-1.0");
+		}
+		
+		//downscale to width=640
+		int iProcessWidth = 1080;
+		
+		Mat matInResized = matInput;
+		if(matInput.width()>iProcessWidth)
+		{
+			matInResized = resizeByWidth(matInput, iProcessWidth);
+		}
+		else
+		{
+			iProcessWidth = matInResized.width();
+		}
+		
+		Mat matBgResized = matBackground;
+		if(matBackground.width()!=iProcessWidth)
+		{
+			matBgResized = resizeByWidth(matBackground, iProcessWidth);
+		}
+		
+		//
+		Mat matDeltaMask = new Mat();
+		try {
+			String sPath = new File(".").getCanonicalPath()+"/test/images/";
+			/**
+			Imgproc.cvtColor( matInResized, matInResized, Imgproc.COLOR_BGR2GRAY );
+			Imgproc.equalizeHist(matInResized, matInResized);
+			saveImageAsFile(matInResized, sPath+"matInResized.jpg");
+			
+			Imgproc.cvtColor( matBgResized, matBgResized, Imgproc.COLOR_BGR2GRAY );
+			Imgproc.equalizeHist(matBgResized, matBgResized);
+			saveImageAsFile(matBgResized, sPath+"matBgResized.jpg");
+			**/
+			
+			Core.absdiff(matBgResized, matInResized, matDeltaMask);
+			
+			//Core.bitwise_and(matBgResized, matInResized, matDeltaMask);			
+			//Core.bitwise_not(matBgResized, matInResized, matDeltaMask);
+			//Core.absdiff(matInResized, matDeltaMask, matDeltaMask);
+		}
+		catch(Throwable t)
+		{
+			t.printStackTrace();
+			matDeltaMask = new Mat(0, 0, matInResized.type());
+		}
+		
+		System.out.println("matDeltaMask="+matDeltaMask.width()+"x"+matDeltaMask.height());
+		System.out.println("matInput="+matInput.width()+"x"+matInput.height());
+		
+		Mat matForeground = new Mat();
+		if(matDeltaMask.width()>0)
+		{
+			Imgproc.GaussianBlur(matDeltaMask, matDeltaMask, new Size(21,21), 0);
+			
+			if(matDeltaMask.channels()>2)
+			{
+				Imgproc.cvtColor(matDeltaMask, matDeltaMask, Imgproc.COLOR_BGRA2GRAY);
+			}
+			Imgproc.threshold(matDeltaMask, matDeltaMask, aDiffThreshold*100, 255, Imgproc.THRESH_BINARY);	
+			//
+			if(matDeltaMask.total()!=matInput.total())
+			{
+				matDeltaMask = resize(matDeltaMask, matInput.width(), matInput.height(), false);
+			}
+			
+			Core.copyTo(matInput, matForeground, matDeltaMask);
+			//
+		}
+		else
+		{
+			matForeground = matInput;
+		}
+		
+		return matForeground;
+	}
+	//
+	public static Mat colorToWhiteMask(Mat aMat)
+	{
+		Mat matMask = new Mat();
+		Imgproc.threshold(aMat, matMask, 5, 255, Imgproc.THRESH_BINARY);
+		return grayscale(matMask);
+	}
+	
+	//
+	
+	public static Mat grayscale(Mat aMat)
+	{
+		Mat matGray = aMat.clone();
+		int iOrigType = aMat.type();
+		
+		if(aMat.channels()==3)
+			Imgproc.cvtColor(aMat, matGray, Imgproc.COLOR_RGB2GRAY);
+		else if(aMat.channels()==4)
+			Imgproc.cvtColor(aMat, matGray, Imgproc.COLOR_RGBA2GRAY);
+		
+		if(iOrigType!=matGray.type())
+		{
+			Imgproc.cvtColor(matGray, matGray, Imgproc.COLOR_GRAY2RGB);
+		}
+		
+		return matGray;
+	}
+	
+	public static Mat solidfill(Mat aMat, Scalar aScalar)
+	{
+		Mat matSolid = new Mat(aMat.size(), aMat.type(), aScalar);
+		
+		Mat matMask = colorToWhiteMask(aMat);
+		
+		Mat matReturn = new Mat();
+		Core.copyTo(matSolid, matReturn, matMask);
+		
+		return matReturn;
+	}
+	
+	public static Mat mediumBlur(Mat aMat, double aBlurScale)
+	{
+		if(aBlurScale>1)
+			aBlurScale = 1;
+		
+		if(aBlurScale<0)
+			aBlurScale = 0;
+		
+		int iBlurScale = (int)(aBlurScale * 100);
+		if(iBlurScale%2==0) iBlurScale += 1;
+		if(iBlurScale>=100) iBlurScale = 99;
+		
+		Mat matBlurred = new Mat();
+		Imgproc.medianBlur(aMat, matBlurred, iBlurScale);
+		
+		return matBlurred;
+	}
+	
+	
+	public static Mat pixelate(Mat aMat, double aPixelateScale)
+	{
+		if(aPixelateScale>1)
+			aPixelateScale = 1;
+		
+		if(aPixelateScale<0)
+			aPixelateScale = 0;
+		
+		int iNewWidth = (int)((1.0 - aPixelateScale) * aMat.width());
+		Mat matPixelated = resizeByWidth(aMat, iNewWidth);
+		return resizeByWidth(matPixelated, aMat.width());
+	}
+	
+	//
+	
+	protected static Mat removeAlphaChannel(Mat matInput)
+	{
+		if(matInput.channels()==4)
+		{
+			Mat matReturn = matInput.clone();
+			Vector<Mat> vRgba = new Vector<Mat>();
+			Core.split(matReturn, vRgba);
+			vRgba.remove(vRgba.size()-1);
+			Core.merge(vRgba, matReturn);
+			return matReturn;
+		}
+		return matInput;
+	}
+	
+	protected static BufferedImage removeAlphaChannel(BufferedImage aImage)
+	{
+		if(aImage.getAlphaRaster()!=null)
+		{
+			BufferedImage imgTmp = new BufferedImage(
+					aImage.getWidth(), aImage.getHeight(), 
+					BufferedImage.TYPE_3BYTE_BGR);
+	
+			Graphics2D g = null;
+			try {
+				g = imgTmp.createGraphics();
+				g.drawImage(aImage, 0, 0, null);
+			}
+			finally
+			{
+				if(g!=null)
+					g.dispose();
+			}
+			return imgTmp;
+		}
+		else
+		{
+			return aImage;			
+		}
+	}
+	
+	public static Mat loadImage(String aImageURI)
+	{
+		return Imgcodecs.imread(aImageURI);
+	}
+	
+	public static void saveImageAsFile(Mat aMatInput, String aFileName)
+	{
+		Imgcodecs.imwrite(aFileName, aMatInput);
+	}
+	
+}
