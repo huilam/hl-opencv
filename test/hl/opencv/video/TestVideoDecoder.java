404d6f0c5fe73f6224855246025d9428385053c6
onghuilam <onghuilam@gmail.com>
2022-11-08 12:32:46 +0800

    update openCv native library for macos and linux

--- test/hl/opencv/video/TestVideoDecoder.java
+++ test/hl/opencv/video/TestVideoDecoder.java
@@ -25,8 +25,12 @@ package hl.opencv.video;
 import java.io.File;
 
 import org.opencv.core.Core;
+import org.opencv.core.DMatch;
 import org.opencv.core.Mat;
-
+import org.opencv.core.MatOfDMatch;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.features2d.DescriptorMatcher;
+import org.opencv.features2d.ORB;
 import hl.opencv.OpenCvLibLoader;
 import hl.opencv.util.OpenCvUtil;
 
@@ -76,9 +80,46 @@ public class TestVideoDecoder extends VideoDecoder {
 		}
 	}
 	
+	public static int compareSimilarity(Mat currentImage, Mat compareImage) {
+
+	    ORB orb = ORB.create();
+	    int similarity = 0;
+
+	    MatOfKeyPoint keypoints1 = new MatOfKeyPoint();
+	    MatOfKeyPoint keypoints2 = new MatOfKeyPoint();
+	    orb.detect(currentImage, keypoints1);
+	    orb.detect(compareImage, keypoints2);
+
+	    Mat descriptors1 = new Mat();
+	    Mat descriptors2 = new Mat();
+	    orb.compute(currentImage, keypoints1, descriptors1);
+	    orb.compute(compareImage, keypoints2, descriptors2);
+
+	    if (descriptors1.cols() == descriptors2.cols()) {
+	        MatOfDMatch matchMatrix = new MatOfDMatch();
+	        DescriptorMatcher matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_HAMMING);
+	        matcher.match(descriptors1, descriptors2, matchMatrix);
+	        DMatch[] matches = matchMatrix.toArray();
+
+	        for (DMatch match : matches)
+	            if (match.distance <= 50)
+	                similarity++;//from   www.java2s.com
+	    }
+	    return similarity;
+	}
+	
 	public static void main(String args[]) throws Exception
 	{
 		initOpenCV();
+		
+		File folder = new File("./test/images/ace");
+		
+		Mat mat1 = OpenCvUtil.loadImage(folder.getAbsolutePath()+"/ace-cam08-dark-01.jpg");
+		Mat mat2 = OpenCvUtil.loadImage(folder.getAbsolutePath()+"/ace-cam08-dark-02.jpg");
+		
+		double dSimilar = TestVideoDecoder.compareSimilarity(mat1, mat2);
+		
+		System.out.println(dSimilar);
 		
 		long lStartMs = System.currentTimeMillis();
 		
@@ -89,9 +130,11 @@ public class TestVideoDecoder extends VideoDecoder {
 		vidDecoder.setMin_brightness_skip_threshold(0.0);
 		vidDecoder.setMin_similarity_skip_threshold(0.0);
 		//
-		vidDecoder.processVideo(file, 5000, 12000);
+		vidDecoder.processVideo(file, 0, 5000);
 		
 		System.out.println();
 		System.out.println("Elapsed : "+(System.currentTimeMillis()-lStartMs)+" ms");
+
+		
 	}
 }
